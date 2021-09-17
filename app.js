174e35631d4ebb8a1438ee8a5fa7046db01dea90
Satyam Pariyar <pariyarsatyam1@gmail.com>
2021-09-17 10:38:40 +0530

    var added in js file

---
+++ app.js
@@ -0,0 +1,5 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputText = document.querySelector("#txt-output");
+
+var serverUrl = "https://api.funtranslations.com/translate/pirate.json";
